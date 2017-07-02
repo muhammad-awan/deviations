f4ea38e80b793fad34f0aceeb99742434c40fb1d
Muhammad Awan <muhammad.adam.awan@gmail.com>
2017-07-03 09:14:33 +1000

    Api: Add restful http actions and routing

---
+++ app.js
@@ -0,0 +1,106 @@
+var express = require('express')
+var app = express()
+var mongoose = require('mongoose')
+var bodyParser = require('body-parser')
+Genre = require('./models/genre')
+Book = require('./models/book')
+
+app.use(bodyParser.json())
+
+mongoose.connect('mongodb://localhost/deviations')
+var db = mongoose.connection
+
+app.get('/', function(req, res){
+    res.send('Hiya')
+})
+
+app.get('/api/genres', function(req, res){
+    Genre.getGenres(function(err, genres){
+        if(err){
+            throw err
+        }
+        res.json(genres)
+    })
+})
+
+app.post('/api/genres', function(req, res){
+    var genre = req.body
+    Genre.addGenre(genre, function(err, genre){
+        if(err){
+            throw err
+        }
+        res.json(genre)
+    })
+})
+
+app.put('/api/genres/:_id', function(req, res){
+    var id = req.params._id
+    var genre = req.body
+    Genre.updateGenre(id, genre, {}, function(err, genre){
+        if(err){
+            throw err
+        }
+        res.json(genre)
+    })
+})
+
+app.delete('/api/genres/:_id', function(req, res){
+    var id = req.params._id
+    Genre.deleteGenre(id, function(err, genre){
+        if(err){
+            throw err
+        }
+        res.json(genre)
+    })
+})
+
+app.get('/api/books', function(req, res){
+    Book.getBooks(function(err, books){
+        if(err){
+            throw err
+        }
+        res.json(books)
+    })
+})
+
+app.get('/api/books/:_id', function(req, res){
+    Book.getBookById(req.params._id, function(err, book){
+        if(err){
+            throw err
+        }
+        res.json(book)
+    })
+})
+
+app.post('/api/books', function(req, res){
+    var book = req.body
+    Book.addBook(book, function(err, book){
+        if(err){
+            throw err
+        }
+        res.json(book)
+    })
+})
+
+app.put('/api/books/:_id', function(req, res){
+    var id = req.params._id
+    var book = req.body
+    Book.updateBook(id, book, {}, function(err, book){
+        if(err){
+            throw err
+        }
+        res.json(book)
+    })
+})
+
+app.delete('/api/books/:_id', function(req, res){
+    var id = req.params._id
+    Book.deleteBook(id, function(err, book){
+        if(err){
+            throw err
+        }
+        res.json(book)
+    })
+})
+
+app.listen(3000)
